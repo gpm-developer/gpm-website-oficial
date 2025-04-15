c72ab680e3dd37945c588f13caf11b6b572eca14
Joseph Vento <desarrolloweb@grupoperezmoreno.com>
2025-04-15 01:45:08 +0100

    testint CI errors 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 export default function Home() {
-  const asd = 1;
-
-  d;
+  const TEST = 1;
 
   return (
     <div className="p-10 text-red-200">
       <p className="text-5xl text-red-500">GPM</p>
       <p className="font-caslon text-5xl italic text-blue-500">testing</p>
-      <p className="font-caslon text-5xl italic text-blue-500">testing 2</p>
+      <p className="font-caslon text-5xl italic text-blue-500">{TEXt}</p>
     </div>
   );
 }
