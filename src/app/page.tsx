d73fa4c0837bceed17196022f4b0c41162dd7d82
Joseph Vento <desarrolloweb@grupoperezmoreno.com>
2025-04-15 01:16:02 +0100

    testing CI 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 export default function Home() {
+  const asd = 1;
+
   return (
     <div className="p-10 text-red-200">
       <p className="text-5xl text-red-500">GPM</p>
       <p className="font-caslon text-5xl italic text-blue-500">testing</p>
-      <p className="font-caslon text-5xl italic text-blue-500">testing x</p>
+      <p className="font-caslon text-5xl italic text-blue-500">testing 2</p>
     </div>
   );
 }
