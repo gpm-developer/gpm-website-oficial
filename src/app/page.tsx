c095208c00324bd6bfab72d0ffb82e31ef374757
Joseph Vento <desarrolloweb@grupoperezmoreno.com>
2025-04-15 01:52:47 +0100

    testinc CI errors 4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="p-10 text-red-200">
       <p className="text-5xl text-red-500">GPM</p>
       <p className="font-caslon text-5xl italic text-blue-500">testing</p>
-      <p className="font-caslon text-5xl italic text-blue-500">{TEXt}</p>
+      <p className="font-caslon text-5xl italic text-blue-500">{TEXtx}</p>
     </div>
   );
 }
